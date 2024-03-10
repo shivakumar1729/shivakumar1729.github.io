78256f843c78ba767b5c6df9bd36c19592402149
SAYYANAG <SAYYANAG@SAYYANAG-7420.in.oracle.com>
2024-03-10 19:37:36 +0530

    initial commit with changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function printToConsole(arg){
+    console.log('🚀 ~  Printing to Console:',arg);
+}
+
+function heartAnimate(){
+    document.getElementById('heartanimation').classList.toggle('animate');
+    console.log('🚀 ~  Heart Clicked and User Loved it!!!');
+    
+}
